b5624c9d4d4132496a9b9fc67a676c11674dd402
sks316 <sks316@mail.com>
2019-11-03 01:52:04 -0500

    Cleanup admin cog and add eval

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,18 +1,23 @@
 import discord
 from discord.ext import commands
 import os
+import io
+import textwrap
+import traceback
+from contextlib import redirect_stdout
 
 botver = "Mewtwo v2.0"
 
 class Admin(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self._last_result = None
 
     @commands.command(pass_context=True)
     @commands.is_owner()
     async def adminhelp(self, ctx):
         embed = discord.Embed(title=botver, description="Administrator commands for Mewtwo. \n The command prefix is `>`. To run a command, you must begin a message with `>`.", color=0x8253c3)
-        embed.add_field(name="Commands:", value="**>shutdown** - Shuts down the bot. Aliases: **>logout** \n**>changestatus** - Changes the bot's Playing status. \n**>reload** - Reloads all cogs.\n**>serverlist** - Outputs a list of servers the bot is in to the terminal. \n**>clearterm** - Clears the terminal.", inline=False)
+        embed.add_field(name="Commands:", value="**>shutdown** - Shuts down the bot. Aliases: **>logout** \n**>changestatus** - Changes the bot's Playing status. \n**>reload** - Reloads all cogs.\n**>serverlist** - Outputs a list of servers the bot is in to the terminal. \n**>clearterm** - Clears the terminal. \n**>eval** - Evaluate provided Python code.", inline=False)
         embed.set_footer(text=botver + " by sks316#2523", icon_url='https://sks316.s-ul.eu/bsHvTCLJ')
         await ctx.message.author.send(embed=embed)
         await ctx.message.add_reaction("✅")
@@ -33,14 +38,56 @@ class Admin(commands.Cog):
         print('------')
         await ctx.send("✅ Done! Check your console!")
 
+    def cleanup_code(self, content):
+        """Automatically removes code blocks from the code."""
+        # remove ```py\n```
+        if content.startswith('```') and content.endswith('```'):
+            return '\n'.join(content.split('\n')[1:-1])
+
+        # remove `foo`
+        return content.strip('` \n')
+
     @commands.command()
     @commands.is_owner()
-    async def announce(self, ctx):
-        embed = discord.Embed(description="I have a small update on the above situation. I've been able to secure hosting for Mewtwo!\nSomeone (I won't say who to protect their privacy) has graciously volunteered to provide hosting for the bot over the next few months! Hopefully uptime won't be as sporadic and we'll experience fewer slowdowns now. You may already notice that some commands such as `>r34` and `>google` are much faster. Once these few months are over I'll have to find another place to host the bot, but let's not worry about that. Let's just enjoy what we have now!\n\nOnce again, thank you for your continued interest in Mewtwo!", color=0x8253c3)
-        embed.set_author(name=botver + " Announcement", icon_url='https://sks316.s-ul.eu/bsHvTCLJ')
-        embed.set_footer(text="Announced by " + ctx.author.name + "#" + ctx.author.discriminator + " - " + botver + " by sks316#2523", icon_url=ctx.author.avatar_url)
-        await ctx.send(embed=embed)
-        await ctx.message.delete()
+    async def eval(self, ctx, *, body: str):
+        msg = await ctx.send("<a:loading:598027019447435285> Evaluating...")
+        env = {
+            'bot': self.bot,
+            'ctx': ctx,
+            'channel': ctx.channel,
+            'author': ctx.author,
+            'guild': ctx.guild,
+            'message': ctx.message,
+            '__': self._last_result
+        }
+        env.update(globals())
+        body = self.cleanup_code(body)
+        stdout = io.StringIO()
+        to_compile = (f'async def func():\n{textwrap.indent(body, "  ")}')
+        try:
+            exec(to_compile, env)
+        except Exception as e:
+            return await msg.edit(content=f'```py\n{e.__class__.__name__}: {e}\n```')
+        func = env['func']
+        try:
+            with redirect_stdout(stdout):
+                ret = await func()
+        except Exception as e:
+            value = stdout.getvalue()
+            await msg.edit(content=f':x: An error occurred while evaluating the code.\nTraceback is shown below.```py\n{value}{traceback.format_exc()}\n```')
+        else:
+            value = stdout.getvalue()
+            try:
+                await msg.delete()
+                await ctx.message.add_reaction('\u2705')
+            except Exception: 
+                pass
+            if ret is None:
+                if value:
+                    await ctx.codeblock(value)
+            else:
+                self._last_result = ret
+                await ctx.codeblock(f"{value}{ret}")
 
     @commands.command()
     @commands.is_owner()
