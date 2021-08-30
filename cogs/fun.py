eb4d47da769e31e2f9d5b4fe6ad208844911d710
sks316 <austins355@yahoo.com>
2021-08-30 04:43:19 -0500

    improved pokedex command

--- cogs/fun.py
+++ cogs/fun.py
@@ -163,59 +163,60 @@ class Fun(commands.Cog):
             async with session.get(f'https://pokeapi.glitch.me/v1/pokemon/{pkmn}') as dex_entry:
                 data = await dex_entry.json()
                 #--Now we attempt to extract information--#
-                try:
-                    pkmn_name = data[0]['name']
-                    pkmn_no = data[0]['number']
-                    pkmn_desc = data[0]['description']
-                    pkmn_img = data[0]['sprite']
-                    pkmn_height = data[0]['height']
-                    pkmn_weight = data[0]['weight']
-                    pkmn_species = data[0]['species']
-                    pkmn_type1 = data[0]['types'][0]
-                    pkmn_gen = str(data[0]['gen'])
-                    pkmn_ability1 = data[0]['abilities']['normal'][0]
-                    #--Detect if Pokemon has a second ability--#
+                for x in data:
                     try:
-                        pkmn_ability2 = data[0]['abilities']['normal'][1]
-                    except IndexError:
-                        pkmn_ability2 = None
-                    #--Detect if Pokemon has a hidden ability--#
-                    try:
-                        pkmn_hiddenability = data[0]['abilities']['hidden'][0]
-                    except IndexError:
-                        pkmn_hiddenability = None
-                    #--Detect if Pokemon has a second type--#
-                    try:
-                        pkmn_type2 = data[0]['types'][1]
-                    except IndexError:
-                        pkmn_type2 = None
-                    #--Finally, we format it into a nice little embed--#
-                    embed = discord.Embed(title=f"<:pokeball:609749611321753669> Pokédex information for {pkmn_name} (#{pkmn_no})", description=pkmn_desc, color=0xd82626)
-                    embed.add_field(name='Height', value=pkmn_height)
-                    embed.add_field(name='Weight', value=pkmn_weight)
-                    embed.add_field(name='Species', value=pkmn_species)
-                    #--Detect if type2 is defined--#
-                    if pkmn_type2 == None:
-                        embed.add_field(name='Type', value=pkmn_type1)
-                    else:
-                        embed.add_field(name='Types', value=f"{pkmn_type1}, {pkmn_type2}")
-                    #--Detect if ability2 and hiddenability defined--#
-                    if pkmn_ability2 == None:
-                        if pkmn_hiddenability == None:
-                            embed.add_field(name='Ability', value=pkmn_ability1)
+                        pkmn_name = x['name']
+                        pkmn_no = x['number']
+                        pkmn_desc = x['description']
+                        pkmn_img = x['sprite']
+                        pkmn_height = x['height']
+                        pkmn_weight = x['weight']
+                        pkmn_species = x['species']
+                        pkmn_type1 = x['types'][0]
+                        pkmn_gen = str(x['gen'])
+                        pkmn_ability1 = x['abilities']['normal'][0]
+                        #--Detect if Pokemon has a second ability--#
+                        try:
+                            pkmn_ability2 = x['abilities']['normal'][1]
+                        except IndexError:
+                            pkmn_ability2 = None
+                        #--Detect if Pokemon has a hidden ability--#
+                        try:
+                            pkmn_hiddenability = x['abilities']['hidden'][0]
+                        except IndexError:
+                            pkmn_hiddenability = None
+                        #--Detect if Pokemon has a second type--#
+                        try:
+                            pkmn_type2 = x['types'][1]
+                        except IndexError:
+                            pkmn_type2 = None
+                        #--Finally, we format it into a nice little embed--#
+                        embed = discord.Embed(title=f"<:pokeball:609749611321753669> Pokédex information for {pkmn_name} (#{pkmn_no})", description=pkmn_desc, color=0xd82626)
+                        embed.add_field(name='Height', value=pkmn_height)
+                        embed.add_field(name='Weight', value=pkmn_weight)
+                        embed.add_field(name='Species', value=pkmn_species)
+                        #--Detect if type2 is defined--#
+                        if pkmn_type2 == None:
+                            embed.add_field(name='Type', value=pkmn_type1)
                         else:
-                            embed.add_field(name='Abilities', value=f"{pkmn_ability1};\n**Hidden:** {pkmn_hiddenability}")
-                    else:
-                        if pkmn_hiddenability == None:
-                            embed.add_field(name='Abilities', value=f"{pkmn_ability1}, {pkmn_ability2}")
+                            embed.add_field(name='Types', value=f"{pkmn_type1}, {pkmn_type2}")
+                        #--Detect if ability2 and hiddenability defined--#
+                        if pkmn_ability2 == None:
+                            if pkmn_hiddenability == None:
+                                embed.add_field(name='Ability', value=pkmn_ability1)
+                            else:
+                                embed.add_field(name='Abilities', value=f"{pkmn_ability1};\n**Hidden:** {pkmn_hiddenability}")
                         else:
-                            embed.add_field(name='Abilities', value=f"{pkmn_ability1}, {pkmn_ability2};\n**Hidden:** {pkmn_hiddenability}")
-                    embed.add_field(name='Generation Introduced', value=f"Gen {pkmn_gen}")
-                    embed.set_thumbnail(url=pkmn_img)
-                    embed.set_footer(text=f"{botver} by PrincessLillie#2523", icon_url='https://sks316.s-ul.eu/bsHvTCLJ')
-                    await ctx.send(embed=embed)
-                except KeyError:
-                    return await ctx.send(":x: I couldn't find any Pokémon with that name. Double-check your spelling and try again. \nIf you're certain that this Pokémon exists, file a bug report with **>bug**.")
+                            if pkmn_hiddenability == None:
+                                embed.add_field(name='Abilities', value=f"{pkmn_ability1}, {pkmn_ability2}")
+                            else:
+                                embed.add_field(name='Abilities', value=f"{pkmn_ability1}, {pkmn_ability2};\n**Hidden:** {pkmn_hiddenability}")
+                        embed.add_field(name='Generation Introduced', value=f"Gen {pkmn_gen}")
+                        embed.set_thumbnail(url=pkmn_img)
+                        embed.set_footer(text=f"{botver} by PrincessLillie#2523", icon_url='https://sks316.s-ul.eu/bsHvTCLJ')
+                        await ctx.send(embed=embed)
+                    except (KeyError, TypeError):
+                        return await ctx.send(":x: I couldn't find any Pokémon with that name. Double-check your spelling and try again. \nIf you're certain that this Pokémon exists, file a bug report with **>bug**.")
 
     @commands.command()
     @commands.cooldown(1, 5, commands.BucketType.user)
